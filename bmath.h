8fb68ce33e235b9ecd2e0897dcd804e2eefe2b74
Nathan Ha <ce.nathan.ha@gmail.com>
2023-01-28 21:21:18 -0800

    Create bmath.h

---
+++ bmath.h
@@ -0,0 +1,55 @@
+/*
+* Finished Functions:
+*/
+
+//just for fun
+void drawSine(); //draws sin(x) in a file called "sin.txt"
+
+//general functions
+double pow(double x, int y);
+// double abs(double x); //doesnt work for some reason
+double fact(int x); //factorial: x!
+double exp(int x);
+double sqrt(double x);
+double nroot(double a, int r); //finds nth root: nroot(a, r) = a^(1/r)
+
+//trig functions
+double sin(double x);
+double cos(double x);
+double tan(double x);
+double sec(double x);
+double csc(double x);
+double cot(double x);
+double dtor(double x); //degrees to radians
+double rtod(double x); //radians to degrees
+
+/*
+* In progress
+*/
+double fpow(double x, double y);
+
+/*
+* Not started yet
+*/
+void printPi();
+double exp(double x);
+double ln(double x);
+double arctan(double x);
+double arcsin(double x);
+double arccos(double x);
+double arcsec(double x);
+double arccsc(double x);
+double arccot(double x);
+
+double sinh(double x);
+double cosh(double x);
+double tanh(double x);
+double sech(double x);
+double csch(double x);
+double coth(double x);
+double arcsinh(double x);
+double arccosh(double x);
+double arctanh(double x);
+double arcsech(double x);
+double arccsch(double x);
+double arccoth(double x);
